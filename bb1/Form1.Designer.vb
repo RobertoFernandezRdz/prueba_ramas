7df9738b901b89c781475d6e7eaf930f18d8b140
RobertoAlfonsoFernandezRodriguez <202207255@mondragonmexico.edu.mx>
2024-04-02 17:09:38 -0600

    add lebel

--- bb1/Form1.Designer.vb
+++ bb1/Form1.Designer.vb
@@ -24,12 +24,13 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Button1 = New Button()
         PictureBox1 = New PictureBox()
+        Label1 = New Label()
         CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' Button1
         ' 
-        Button1.Location = New Point(192, 109)
+        Button1.Location = New Point(12, 48)
         Button1.Name = "Button1"
         Button1.Size = New Size(172, 142)
         Button1.TabIndex = 0
@@ -39,27 +40,39 @@ Partial Class Form1
         ' PictureBox1
         ' 
         PictureBox1.Image = My.Resources.Resources.nationalgeographic_1468962
-        PictureBox1.Location = New Point(396, 82)
+        PictureBox1.Location = New Point(449, 135)
         PictureBox1.Name = "PictureBox1"
         PictureBox1.Size = New Size(230, 185)
         PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
         PictureBox1.TabIndex = 1
         PictureBox1.TabStop = False
         ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(468, 62)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(26, 15)
+        Label1.TabIndex = 2
+        Label1.Text = "kira"
+        ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(Label1)
         Controls.Add(PictureBox1)
         Controls.Add(Button1)
         Name = "Form1"
         Text = "Form1"
         CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
     Friend WithEvents Button1 As Button
     Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
 
 End Class
